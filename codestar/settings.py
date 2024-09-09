c13808ca06ce36ba722582a89ac9bb74ef0c50fa
Jack Hunt <kegz4uv6@students.codeinstitute.net>
2024-09-09 14:52:46 +0000

    fix missing MIDDLEWARE mesage

--- codestar/settings.py
+++ codestar/settings.py
@@ -47,7 +47,8 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware'
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'codestar.urls'
