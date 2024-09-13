9bdb94c28c3c28d1534a42e9e56dbec5d3da1a15
Jack Hunt <kegz4uv6@students.codeinstitute.net>
2024-09-13 14:29:26 +0000

    fix views post_detail indentation

--- blog/views.py
+++ blog/views.py
@@ -11,23 +11,21 @@ class PostList(generic.ListView):
 
 
 
-    def post_detail(request, slug):
-        """
-        Display an individual :model:`blog.Post`.
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
 
-        **Context**
+    **Context**
 
-        ``post``
-           An instance of :model:`blog.Post`.
+    ``post``
+       An instance of :model:`blog.Post`.
 
-        **Template:**
+    **Template:**
 
-        :template:`blog/post_detail.html`
-        """
-
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-
+    :template:`blog/post_detail.html
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
     return render(
         request,
         "blog/post_detail.html",
